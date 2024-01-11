265e2776cfedcbd846771d77d7e745e81955a023
Onel <113157468+onelmachiz@users.noreply.github.com>
2024-01-11 17:38:57 -0400

    Add files via upload

---
+++ l10n_ve_withholding/views/l10n_ve_withholding_custom/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Localización Withholding Venezuela Custom",
+    'description': """
+        **Localización VENEZUELA Withholding**
+    """,
+
+    'author': "Moises Edson",
+    'website': "",
+    'version': '16.0.0.1',
+    'category': 'Localization',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_ve_withholding',
+    ],
+    'data': [
+        'views/report_withholding_certificate.xml',
+        'views/report_withholding_certificate_iva.xml',
+    ],
+
+}
